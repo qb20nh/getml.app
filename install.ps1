8c26eb85244c773c9091b57359c354e62f137bb2
Taejong Yoo <yut951121@gmail.com>
2022-09-24 05:09:04 +0900

    mime text/pain

--- install.ps1
+++ install.ps1
@@ -1,3 +1,8 @@
+#<!--
+<#--><head>
+    <meta http-equiv="content-type" content="text/plain">
+</head><!--#>#-->
+
 Set-Location ~/Downloads
 
 Invoke-WebRequest https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Windows-x86_64.exe -OutFile miniforge3.exe
