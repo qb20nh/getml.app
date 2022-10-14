cfc9fdbe11a29d8285b645ff2e8ff9dc64c99360
Taejong Yoo <yut951121@gmail.com>
2022-10-14 16:14:21 +0900

    rdp port

--- configure_network.ps1
+++ configure_network.ps1
@@ -1,8 +1,10 @@
 Add-LocalGroupMember -Group "Users" -Member "Server"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "Server"
 
-New-NetFirewallRule -Name RDP_UDP_OPEN -Protocol UDP -RemotePort 1521 -Enabled True -Action Allow -Profile Any -DisplayName "RDP UDP Open"
-New-NetFirewallRule -Name RDP_TCP_OPEN -Protocol TCP -RemotePort 1521 -Enabled True -Action Allow -Profile Any -DisplayName "RDP TCP Open"
+New-NetFirewallRule -Name RDP_UDP_OPEN -Protocol UDP -LocalPort 1521 -Enabled True -Action Allow -Profile Any -DisplayName "RDP UDP Open"
+New-NetFirewallRule -Name RDP_TCP_OPEN -Protocol TCP -LocalPort 1521 -Enabled True -Action Allow -Profile Any -DisplayName "RDP TCP Open"
 
 $idx = Get-NetConnectionProfile | Select-Object -ExpandProperty InterfaceIndex
 Set-NetConnectionProfile -InterfaceIndex $idx -NetworkCategory Private
+
+New-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name PortNumber -Value 1521 -PropertyType DWORD -Force
