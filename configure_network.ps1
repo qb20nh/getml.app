ecef46f7e8cebec0f401da32991f0ecbc1c6b03c
Taejong Yoo <yut951121@gmail.com>
2022-10-14 15:31:12 +0900

    network fixer

---
+++ configure_network.ps1
@@ -0,0 +1,7 @@
+Add-LocalGroupMember -Group "Users" -Member "Server"
+
+New-NetFirewallRule -Name RDP_UDP_OPEN -Protocol UDP -RemotePort 1521 -Enabled True -Action Allow -Profile Any -DisplayName "RDP UDP Open"
+New-NetFirewallRule -Name RDP_TCP_OPEN -Protocol TCP -RemotePort 1521 -Enabled True -Action Allow -Profile Any -DisplayName "RDP TCP Open"
+
+$idx = Get-NetConnectionProfile | Select-Object -ExpandProperty InterfaceIndex
+Set-NetConnectionProfile -InterfaceIndex $idx -NetworkCategory Private
