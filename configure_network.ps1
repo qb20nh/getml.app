3bdd3e497f9b0ad06e16d6edbdb6e1fbc635879a
Taejong Yoo <yut951121@gmail.com>
2022-11-01 18:17:11 +0900

    nonfailure auto configuration

--- configure_network.ps1
+++ configure_network.ps1
@@ -1,10 +1,23 @@
+# Add user to local group to enable logging in with RDP
 Add-LocalGroupMember -Group "Users" -Member "Server"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "Server"
 
-New-NetFirewallRule -Name RDP_UDP_OPEN -Protocol UDP -LocalPort 1521 -Enabled True -Action Allow -Profile Any -DisplayName "RDP UDP Open"
-New-NetFirewallRule -Name RDP_TCP_OPEN -Protocol TCP -LocalPort 1521 -Enabled True -Action Allow -Profile Any -DisplayName "RDP TCP Open"
+$rdPort = 1521
 
+# Firewall rules
+Remove-NetFirewallRule -Name RDP_UDP_OPEN
+New-NetFirewallRule -Name RDP_UDP_OPEN -Protocol UDP -LocalPort $rdPort -Enabled True -Action Allow -Profile Any -DisplayName "RDP UDP Open"
+Remove-NetFirewallRule -Name RDP_TCP_OPEN
+New-NetFirewallRule -Name RDP_TCP_OPEN -Protocol TCP -LocalPort $rdPort -Enabled True -Action Allow -Profile Any -DisplayName "RDP TCP Open"
+
+# Allow local connections
 $idx = Get-NetConnectionProfile | Select-Object -ExpandProperty InterfaceIndex
 Set-NetConnectionProfile -InterfaceIndex $idx -NetworkCategory Private
 
-New-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name PortNumber -Value 1521 -PropertyType DWORD -Force
+# Set listening port for remote desktop services
+New-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name PortNumber -Value $rdPort -PropertyType DWORD -Force
+
+# Start related services
+$nameWildcard = "*remote desktop*"
+Get-Service -DisplayName $nameWildcard | Select-Object -ExpandProperty Name | ForEach-Object -Process {Set-Service -Name $_ -StartupType Automatic}
+Start-Service -DisplayName $nameWildcard
