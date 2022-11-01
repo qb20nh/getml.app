9862e424dfb25461fcffa9aafd326606093bba46
Taejong Yoo <yut951121@gmail.com>
2022-11-01 18:26:39 +0900

    plz work plz

--- configure_remote_desktop.ps1
+++ configure_remote_desktop.ps1
@@ -1,10 +1,11 @@
-# Add user to local group to enable logging in with RDP
-Add-LocalGroupMember -Group "Users" -Member "Server"
-Add-LocalGroupMember -Group "Remote Desktop Users" -Member "Server"
-
 $rdPort = 1521
 
-# Firewall rules
+# Add user to local group to enable logging in with RDP
+$username = "Server"
+Add-LocalGroupMember -Group "Users" -Member $username
+Add-LocalGroupMember -Group "Remote Desktop Users" -Member $username
+
+# Add firewall rules
 Remove-NetFirewallRule -Name RDP_UDP_OPEN
 New-NetFirewallRule -Name RDP_UDP_OPEN -Protocol UDP -LocalPort $rdPort -Enabled True -Action Allow -Profile Any -DisplayName "RDP UDP Open"
 Remove-NetFirewallRule -Name RDP_TCP_OPEN
@@ -17,7 +18,7 @@ Set-NetConnectionProfile -InterfaceIndex $idx -NetworkCategory Private
 # Set listening port for remote desktop services
 New-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name PortNumber -Value $rdPort -PropertyType DWORD -Force
 
-# Start related services
+# Enable and restart related services
 $nameWildcard = "*remote desktop*"
 Get-Service -DisplayName $nameWildcard | Select-Object -ExpandProperty Name | ForEach-Object -Process {Set-Service -Name $_ -StartupType Automatic}
 Start-Service -DisplayName $nameWildcard
